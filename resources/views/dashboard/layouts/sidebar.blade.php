2bdee36be736a2ae5390beae04f7fcbc5d2523dc
mzaedan <zaedanm108@gmail.com>
2021-09-16 07:47:50 +0700

    update

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -8,7 +8,7 @@
         </a>
         </li>
         <li class="nav-item">
-        <a class="nav-link {{ Request::is('dashboard/posts') ? 'active' : '' }}" href={{ url('/dashboard/posts')  }}>
+        <a class="nav-link {{ Request::is('dashboard/posts*') ? 'active' : '' }}" href={{ url('/dashboard/posts')  }}>
             <span data-feather="file-text"></span>
             My Pots
         </a>
