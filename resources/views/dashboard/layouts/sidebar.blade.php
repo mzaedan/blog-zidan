0ed68eba1104220266627ab63b7aeaa2ada558ad
mzaedan <zaedanm108@gmail.com>
2021-09-30 20:30:23 +0700

    update

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -14,5 +14,23 @@
         </a>
         </li>
     </ul>
+
+    <h6 class="sidebar-heading d-flex justtify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+        <span>administrator</span>
+    </h6>
+
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }}" href={{ url('/dashboard/categories')  }}>
+                <span data-feather="grid"></span>
+                Post Categories 
+            </a>
+        </li>
+    </ul>
+
+
+
+
+
     </div>
 </nav>
