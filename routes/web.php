75db9bc9deb7db7631ce5bf892e020eec92c1906
mzaedan <zaedanm108@gmail.com>
2022-04-27 09:17:33 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -93,5 +93,5 @@ Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'check
     
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
-Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show');
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
 
